19f29a0fba15dcbfc9b6f14e479d2eff9f5f01f3
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-04-06 23:59:14 +0300

    added test for stack and added class Stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+from src.stack import Stack
+def test_stack(some_stack):
+
+    assert some_stack.top == None
+
+def test_stack_push(some_stack):
+    some_stack.push(4)
+    assert some_stack.top.data == 4
+
+
+def test_stack_pop(some_stack):
+    assert some_stack.pop() == None
+    some_stack.push(1)
+    some_stack.push(2)
+    del_data = some_stack.pop()
+    assert del_data == 2
+
+
