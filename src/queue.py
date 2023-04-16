985fde9868568ba79f13a3f0d7a07cd5995fdb89
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-04-16 22:25:13 +0300

    added new struckt queue

--- src/queue.py
+++ src/queue.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,12 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        node = Node(data, None)
+        if not self.tail:
+            self.head = node
+        else:
+            self.tail.next_node = node
+        self.tail = node
 
     def dequeue(self):
         """
@@ -35,4 +42,13 @@ class Queue:
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        res = ""
+        if self.head == None:
+            return ""
+        while self.head:
+            res += self.head.data +'\n'
+            self.head = self.head.next_node
+
+        return res
+
+
