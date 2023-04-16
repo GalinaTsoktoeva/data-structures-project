b959d9a7004f3d490ab5c81c3f7daa8c91a4a2a9
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-04-16 22:43:00 +0300

    added test for queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+def test_queue(some_queue):
+    assert some_queue.head == None
+    assert some_queue.tail == None
+
+def test_enqueue(some_queue):
+    some_queue.enqueue("data1")
+    some_queue.enqueue("data2")
+    some_queue.enqueue("data3")
+    assert some_queue.head.data == "data1"
+    assert some_queue.tail.data == "data3"
+
+def test_str(some_queue):
+    assert str(some_queue) == ""
+    some_queue.enqueue("data1")
+    assert str(some_queue) == "data1\n"
+
