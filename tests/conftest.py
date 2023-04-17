b192c2db7da860d2f8948481ac5817dcd68eaada
gtsoktoeva001 <galina-tsoktoeva@mail.ru>
2023-04-17 19:43:19 +0300

    add queue to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from src.stack import Stack
+from src.queue import Queue
 
 @pytest.fixture()
 def some_stack():
     return Stack()
+
+@pytest.fixture()
+def some_queue():
+    return Queue()
